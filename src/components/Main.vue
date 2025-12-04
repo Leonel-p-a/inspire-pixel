698a972c6e0c034aad89e39dcf63e2e0f0435cf5
Leonel Almeida <leonelalmeida.10.la@gmail.com>
2025-12-04 01:06:58 -0300

    feat: implemented fetch method for consuming API image service on the Main component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,44 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import Card from './Card.vue';
+
+const photos = ref([]);
+const errorMessage = ref(null);
+
+async function fetchData() {
+    try {
+        const response = await fetch('https://picsum.photos/v2/list');
+
+        if (!response.ok) {
+            throw {
+                status: response.status,
+                message: response.statusText || "Erro desconhecido"
+            };
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        errorMessage.value = `Erro ${error.status}: ${error.message}`;
+    }
+}
+
+onMounted(async () => {
+    photos.value = await fetchData();
+});
 </script>
 
 <template>
     <section class="cards-container">
         <h2>Inspire-se</h2>
 
+        <div v-if="errorMessage" class="error-box">
+            <h2>{{ errorMessage }}</h2>
+            <p>Tente novamente mais tarde.</p>
+        </div>
+
         <div class="cards-grid">
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
-            <Card img="https://wallpapers.com/images/featured/fundo-de-paisagem-natural-n1kwq7ikw7uu0jgx.jpg" />
+            <Card v-for="photo in photos" :key="photo.id" :img="photo.download_url" />
         </div>
     </section>
 </template>
@@ -27,12 +47,25 @@ import Card from './Card.vue';
 @use '@/styles/variables.scss' as vars;
 
 .cards-container {
-    
+
     h2 {
         font-size: 3rem;
         font-weight: 400;
         margin-bottom: 25px;
         font-family: vars.$primary-font;
+    }
+
+    .error-box {
+        padding: 20px;
+        background: #ffebeb;
+        color: #b30000;
+        border: 1px solid #b30000;
+        border-radius: 8px;
+        margin-bottom: 20px;
+
+        p {
+            font-size: 1.6rem;
+        }
     }
 
     .cards-grid {
